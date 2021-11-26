5e257012c8b7beac5a70c7626051dfee08db6350
zourongchuan <pipinet@vip.qq.com>
2021-11-27 00:32:05 +0800

    init

---
+++ auditing/src/main/java/com/qwlabs/doraemon/auditing/AuditingListener.java
@@ -0,0 +1,50 @@
+package com.qwlabs.doraemon.auditing;
+
+import io.quarkus.security.identity.SecurityIdentity;
+
+import javax.enterprise.inject.spi.CDI;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.Clock;
+import java.time.Instant;
+import java.util.Optional;
+
+public class AuditingListener {
+    @PrePersist
+    public void prePersist(Object entity) {
+        if (entity instanceof CreatedAuditedEntity) {
+            touchCreated((CreatedAuditedEntity) entity);
+        }
+        if (entity instanceof AuditedEntity) {
+            touchUpdated((AuditedEntity) entity);
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate(Object entity) {
+        if (entity instanceof AuditedEntity) {
+            touchUpdated((AuditedEntity) entity);
+        }
+    }
+
+    private static void touchCreated(CreatedAuditedEntity entity) {
+        entity.setCreatedAt(Optional.ofNullable(entity.getCreatedAt())
+                .orElseGet(() -> Instant.now(Clock.systemUTC())));
+        entity.setCreatedBy(Optional.ofNullable(entity.getCreatedBy())
+                .orElseGet(() -> getAuditorId().orElse(null)));
+    }
+
+    private static void touchUpdated(AuditedEntity entity) {
+        entity.setUpdatedAt(Instant.now(Clock.systemUTC()));
+        entity.setUpdatedBy(Optional.ofNullable(entity.getUpdatedBy())
+                .orElseGet(() -> getAuditorId().orElse(null)));
+    }
+
+    private static Optional<String> getAuditorId() {
+        SecurityIdentity identity = CDI.current().select(SecurityIdentity.class).get();
+        if (identity.isAnonymous()) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(identity.getPrincipal().getName());
+    }
+}
---
+++ panache/src/main/java/com/qwlabs/doraemon/panache/Ranged.java
@@ -0,0 +1,52 @@
+package com.qwlabs.doraemon.panache;
+
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.panache.common.Range;
+import io.smallrye.common.constraint.Nullable;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class Ranged<T> {
+    private final Stream<T> data;
+    private final Long totalCount;
+    private final Range range;
+
+    public Ranged(Stream<T> data, Long totalCount, Range range) {
+        this.data = data;
+        this.totalCount = totalCount;
+        this.range = range;
+    }
+
+    public Stream<T> getData() {
+        return this.data;
+    }
+
+    public Long getTotalCount() {
+        return this.totalCount;
+    }
+
+    public Range getRange() {
+        return range;
+    }
+
+    public boolean hasPreviousPage() {
+        return this.totalCount > 0 && range.getStartIndex() > 1;
+    }
+
+    public boolean hasNextPage() {
+        return range.getLastIndex() < this.totalCount;
+    }
+
+    public <R> Ranged<R> map(@Nullable Function<T, R> mapper) {
+        return Optional.ofNullable(mapper)
+                .map(m -> new Ranged<>(data.map(mapper), totalCount, range))
+                .orElseGet(() -> (Ranged<R>) this);
+    }
+
+    public static <T> Ranged<T> of(PanacheQuery<T> pageQuery, Range range) {
+        return new Ranged<>(pageQuery.range(range.getStartIndex(), range.getLastIndex()).stream(),
+                pageQuery.count(), range);
+    }
+}
