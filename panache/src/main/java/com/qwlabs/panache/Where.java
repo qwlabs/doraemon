81126d2ceb050c833238dc8b9413365c2644b413
zourongchuan <pipinet@vip.qq.com>
2022-02-04 23:30:51 +0800

    add panache where

---
+++ panache/src/main/java/com/qwlabs/panache/Where.java
@@ -0,0 +1,88 @@
+package com.qwlabs.panache;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public final class Where {
+    private static final String AND = "and";
+    private static final String OR = "or";
+
+    private StringBuilder where = new StringBuilder();
+    private Map<String, Object> parameters = new HashMap<>();
+
+    private Where() {
+    }
+
+    public static Where create() {
+        return new Where();
+    }
+
+    public Where and(Where where) {
+        Objects.requireNonNull(where, "where can not be null.");
+        return with(AND, where);
+    }
+
+    public Where or(Where where) {
+        Objects.requireNonNull(where, "where can not be null.");
+        return with(OR, where);
+    }
+
+    public Where and(String expression, String name, Object value) {
+        Objects.requireNonNull(expression, "expression can not be null.");
+        Objects.requireNonNull(name, "name can not be null.");
+        return with(AND, expression, name, value);
+    }
+
+    public Where or(String expression, String name, Object value) {
+        Objects.requireNonNull(expression, "expression can not be null.");
+        Objects.requireNonNull(name, "name can not be null.");
+        return with(OR, expression, name, value);
+    }
+
+    private Where with(String operator, Where where) {
+        if (where.isEmpty()) {
+            return this;
+        }
+        if (this.isEmpty()) {
+            return where;
+        }
+        this.pack().where.append(operator).append(where.pack().where);
+        this.parameters.putAll(where.parameters);
+        return this;
+    }
+
+    private Where append(String operator) {
+        if (where.length() > 0) {
+            where.append(" ").append(operator).append(" ");
+        }
+        return this;
+    }
+
+    private Where pack() {
+        where.insert(0, "(").append(")");
+        return this;
+    }
+
+    private Where with(String operator, String expression, String name, Object value) {
+        if (value == null) {
+            return this;
+        }
+        this.append(operator).where.append(expression);
+        parameters.put(name, value);
+        return this;
+    }
+
+    public boolean isEmpty() {
+        return this.where.length() == 0 && parameters.isEmpty();
+    }
+
+    public String get() {
+        return where.toString();
+    }
+
+    public Map<String, Object> getParameters() {
+        return Collections.unmodifiableMap(parameters);
+    }
+}
