c876df516fe1a2b5d68810305653d644d6a89f36
zourongchuan <pipinet@vip.qq.com>
2022-06-16 00:17:24 +0800

    upgrade version

--- auditing/src/main/java/com/qwlabs/auditing/AuditedEntity.java
+++ auditing/src/main/java/com/qwlabs/auditing/AuditedEntity.java
@@ -1,28 +1,17 @@
 package com.qwlabs.auditing;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.Instant;
 
+@Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingListener.class)
 public class AuditedEntity extends CreatedAuditedEntity {
     private Instant updatedAt;
     private String updatedBy;
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public String getUpdatedBy() {
-        return updatedBy;
-    }
-
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
 }
--- auditing/src/main/java/com/qwlabs/auditing/CreatedAuditedEntity.java
+++ auditing/src/main/java/com/qwlabs/auditing/CreatedAuditedEntity.java
@@ -1,28 +1,17 @@
 package com.qwlabs.auditing;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.Instant;
 
+@Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingListener.class)
 public class CreatedAuditedEntity {
     private Instant createdAt;
     private String createdBy;
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }
 }
