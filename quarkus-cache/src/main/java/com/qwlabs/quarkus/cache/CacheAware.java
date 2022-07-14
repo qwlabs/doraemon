d22cf357d0dd6c456ee15c6ef69e57de246db49a
zourongchuan <pipinet@vip.qq.com>
2022-07-14 16:07:51 +0800

    add quarkus cache

---
+++ quarkus-cache/src/main/java/com/qwlabs/quarkus/cache/CacheAware.java
@@ -0,0 +1,21 @@
+package com.qwlabs.quarkus.cache;
+
+import com.qwlabs.cdi.SafeCDI;
+import io.quarkus.cache.Cache;
+import io.quarkus.cache.CacheManager;
+import io.quarkus.cache.runtime.noop.NoOpCache;
+
+import javax.enterprise.inject.Instance;
+
+public interface CacheAware {
+    Cache DEFAULT = new NoOpCache();
+
+    default Cache getCache(String cacheName) {
+        return SafeCDI.current()
+                .map(cdi -> cdi.select(CacheManager.class))
+                .filter(Instance::isResolvable)
+                .map(Instance::get)
+                .flatMap(cacheManager -> cacheManager.getCache(cacheName))
+                .orElse(DEFAULT);
+    }
+}
