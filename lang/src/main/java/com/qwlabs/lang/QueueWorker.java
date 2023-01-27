3594782929af892331af9562631c95551d2aba31
zourongchuan <pipinet@vip.qq.com>
2023-01-27 20:46:09 +0800

    chore: add queue worker default context

--- lang/src/main/java/com/qwlabs/lang/QueueWorker.java
+++ lang/src/main/java/com/qwlabs/lang/QueueWorker.java
@@ -1,13 +1,16 @@
 package com.qwlabs.lang;
 
+import com.google.common.collect.Sets;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.time.Duration;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -160,5 +163,22 @@ public class QueueWorker<C, E> {
     @FunctionalInterface
     public interface FailedConsumer<C, E> {
         void accept(C context, E element, Exception e);
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class StopRoundContext {
+        private final String topic;
+        private final Duration timeout = Duration.ofHours(1);
+        private final Set<String> failedRecordIds = Sets.newHashSet();
+
+        public void markFailedRecord(String recordId) {
+            this.failedRecordIds.add(recordId);
+        }
+
+        public boolean shouldContinue(String recordId) {
+            return !this.failedRecordIds.contains(recordId);
+        }
     }
 }
