e8ed66ff8508862793be3ec5978bd22b436ac8cb
zourongchuan <pipinet@vip.qq.com>
2023-03-14 12:11:59 +0800

    chore: bump gradle version to 7.6.1

--- jackson/src/main/java/com/qwlabs/jackson/Jackson.java
+++ jackson/src/main/java/com/qwlabs/jackson/Jackson.java
@@ -7,8 +7,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.qwlabs.cdi.SafeCDI;
+import jakarta.annotation.Nullable;
 import jakarta.enterprise.inject.Instance;
-import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
@@ -91,7 +91,10 @@ public final class Jackson {
         }
     }
 
-    public static String asText(@NotNull ObjectNode node, @NotNull String propertyName) {
+    public static String asText(@Nullable ObjectNode node, @Nullable String propertyName) {
+        if (node == null || propertyName == null) {
+            return null;
+        }
         return Optional.ofNullable(node.get(propertyName))
                 .map(JsonNode::asText)
                 .orElse(null);
