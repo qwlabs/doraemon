86f57beeea24f5eaefc66550099920a9dc399bc9
zourongchuan <pipinet@vip.qq.com>
2023-04-12 11:10:03 +0800

    chore: tenant

--- quarkus-tenant/src/main/java/com/qwlabs/quarkus/tenant/Tenant.java
+++ quarkus-tenant/src/main/java/com/qwlabs/quarkus/tenant/Tenant.java
@@ -1,16 +1,28 @@
 package com.qwlabs.quarkus.tenant;
 
+import com.google.common.base.Strings;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 
 public interface Tenant {
+
+    @Nullable
+    default boolean isEmpty() {
+        return Strings.isNullOrEmpty(tenantId());
+    }
+    
+    @Nullable
+    default boolean isPresent() {
+        return !isEmpty();
+    }
+
     @Nullable
     String tenantId();
 
     @Nullable
     <T> T attribute(@NotNull String name);
 
-    @NotNull  TenantConfig config();
+    @NotNull TenantConfig config();
 
     default boolean enabled() {
         return config().enabled();
