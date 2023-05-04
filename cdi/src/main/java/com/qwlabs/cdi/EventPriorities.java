e177e380cb2762020e6acf779553769dc709dcd6
zourongchuan <pipinet@vip.qq.com>
2023-05-04 11:42:51 +0800

    chore: add EventPriorities

---
+++ cdi/src/main/java/com/qwlabs/cdi/EventPriorities.java
@@ -0,0 +1,8 @@
+package com.qwlabs.cdi;
+
+public class EventPriorities {
+    public static final int CRITICAL = 3000;
+    public static final int HIGH = 2000;
+    public static final int NORMAL = 1000;
+    public static final int LOW = 0;
+}
