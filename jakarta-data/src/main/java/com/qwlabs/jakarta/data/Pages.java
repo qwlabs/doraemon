f2db2e5f999c6739d27514c30773445e9d7cb07f
pipinet <zourongchuan@handaotech.com>
2024-10-09 18:07:31 +0800

    chore: add pages

---
+++ jakarta-data/src/main/java/com/qwlabs/jakarta/data/Pages.java
@@ -0,0 +1,14 @@
+package com.qwlabs.jakarta.data;
+
+import jakarta.data.page.Page;
+import jakarta.data.page.impl.PageRecord;
+
+import java.util.List;
+import java.util.function.Function;
+
+public interface Pages {
+    static <E, R> Page<R> map(Page<E> page, Function<E, R> mapper) {
+        List<R> content = page.stream().map(mapper).toList();
+        return new PageRecord<>(page.pageRequest(), content, page.totalElements());
+    }
+}
